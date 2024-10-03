ff15410b8b654ccdcefca981d119064fcfa7c4be
Buianhvu226 <buianhvu226@gmail.com>
2024-10-03 23:49:53 +0700

    Fix rename

--- frontend-web/src/pages/authen/LoginPage.js
+++ frontend-web/src/pages/authen/LoginPage.js
@@ -2,11 +2,10 @@ import LoginForm from "../../components/auth/LoginForm";
 import Header from "../../components/header/Header";
 const LoginPage = () => {
   return (
-
-      <div className="bg-gray-200 min-h-screen">
-        <Header />
-        <LoginForm />
-      </div>
+    <div className="bg-gray-200 min-h-screen">
+      <Header />
+      <LoginForm />
+    </div>
   );
 };
 
