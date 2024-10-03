c81bd7286374b23997f69aa65d36ce37cb25ea8a
Buianhvu226 <buianhvu226@gmail.com>
2024-10-03 20:00:03 +0700

    Login Form

--- frontend-web/src/App.js
+++ frontend-web/src/App.js
@@ -2,17 +2,14 @@ import './App.css';
 import Header from './components/Header/header';
 import Footer from './components/Footer/footer'
 import Introduce from './components/Introduce/introduce';
+import LoginForm from './components/Auth/LoginForm';
 
 function App() {
   return (
     <div className="flex flex-col min-h-screen">
         <Header />
-        <main className="flex-grow">
-            <div className="p-10 text-center">
-                <p>Hello 123</p>
-            </div> 
-        </main>
-        <Introduce />  
+            <LoginForm />
+            <Introduce />  
         <Footer />
     </div>
 );
