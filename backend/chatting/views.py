8dcb446fa7bd2d5b5a00a86d39d45dfc384e31c1
NhatQuang1501 <nhatquang1512003@gmail.com>
2024-11-18 02:58:57 +0700

    fix: create chatroom function, admin django view

--- backend/chatting/views.py
+++ backend/chatting/views.py
@@ -18,16 +18,15 @@ from django.shortcuts import get_object_or_404
 from django.utils import timezone
 from application.utils import *
 from django.db.models import Q, Count, F
-from datetime import timedelta
+from datetime import timedelta, datetime
 
 
-# APIView cho ChatRoom
 class ChatRoomView(APIView):
     permission_classes = [IsAuthenticated, IsAdminOrUser]
 
     def get(self, request, chatroom_id=None):
         if chatroom_id:
-            """Lấy thông tin chi tiết phòng chat"""
+            """Lấy chi tiết phòng chat"""
             chatroom = get_object_or_404(ChatRoom, chatroom_id=chatroom_id)
 
             # Kiểm tra quyền tham gia
@@ -42,12 +41,16 @@ class ChatRoomView(APIView):
             return Response(serializer.data, status=status.HTTP_200_OK)
 
         else:
-            """Lấy danh sách tất cả các phòng chat mà user đã tham gia"""
+            """Lấy danh sách tất cả các phòng chat của 1 user"""
             params = {key.strip(): value for key, value in request.query_params.items()}
             user_id = params.get("user_id").strip()
             user = User.objects.get(user_id=user_id)
 
-            chatrooms = ChatRoom.objects.filter(participants__in=[user]).distinct()
+            chatrooms = (
+                ChatRoom.objects.filter(participants__in=[user])
+                .distinct()
+                .order_by("-created_at")
+            )
             serializer = ChatRoomSerializer(chatrooms, many=True)
 
             return Response(
@@ -56,7 +59,7 @@ class ChatRoomView(APIView):
             )
 
     def post(self, request):
-        """Tạo một phòng chat mới."""
+        """Tạo phòng chat mới"""
         data = request.data
         is_private = data.get("is_private", False)
         participants = data.get("participants", [])
@@ -72,17 +75,44 @@ class ChatRoomView(APIView):
             user1 = request.user
             user2 = User.objects.get(user_id=participants[0])
 
+            if user1 == user2:
+                return Response(
+                    {"message": "Không thể tạo phòng chat với chính mình."},
+                    status=status.HTTP_400_BAD_REQUEST,
+                )
+
+            # Kiểm tra xem phòng chat riêng tư giữa hai người đã tồn tại chưa
+            if (
+                ChatRoom.objects.filter(is_private=True, participants=user1)
+                .filter(participants=user2)
+                .exists()
+            ):
+                return Response(
+                    {"message": "Phòng chat riêng tư giữa hai người đã tồn tại"},
+                    status=status.HTTP_400_BAD_REQUEST,
+                )
+
             # Kiểm tra xem họ đã là bạn bè chưa
             if (
                 Friendship.objects.filter(user1=user1, user2=user2).exists()
                 or Friendship.objects.filter(user1=user2, user2=user1).exists()
             ):
-                chatroom = ChatRoom.objects.create(created_by=user1, is_private=True)
+                chatroom = ChatRoom.objects.create(
+                    created_by=user1,
+                    is_private=True,
+                    chatroom_name=f"{user1.username} & {user2.username}",
+                )
                 chatroom.participants.set([user1, user2])
                 chatroom.save()
                 serializer = ChatRoomSerializer(chatroom)
 
-                return Response(serializer.data, status=status.HTTP_201_CREATED)
+                return Response(
+                    {
+                        "message": f"Tạo phòng chat riêng tư thành công giữa {user1.username} và {user2.username}",
+                        "data": serializer.data,
+                    },
+                    status=status.HTTP_201_CREATED,
+                )
 
             else:
                 return Response(
@@ -90,11 +120,26 @@ class ChatRoomView(APIView):
                     status=status.HTTP_400_BAD_REQUEST,
                 )
 
+        # Kiểm tra xem tên nhóm chat đã tồn tại chưa
+        chatroom_name = data.get(
+            "chatroom_name",
+            f"Phòng chat {datetime.now().strftime('%d/%m/%Y %H:%M:%S')}",
+        )
+        if ChatRoom.objects.filter(
+            created_by=request.user, chatroom_name=chatroom_name
+        ).exists():
+            return Response(
+                {
+                    "message": "Tên nhóm chat này đã được bạn sử dụng cho nhóm chat khác. Vui lòng chọn tên khác."
+                },
+                status=status.HTTP_400_BAD_REQUEST,
+            )
+
         # Tạo phòng chat nhóm
         chatroom = ChatRoom.objects.create(
             created_by=request.user,
             is_private=False,
-            chatroom_name=data.get("chatroom_name", "Group Chat"),
+            chatroom_name=chatroom_name,
         )
 
         # Kiểm tra từng participant xem họ có phải là bạn bè của người tạo hay không
@@ -185,7 +230,7 @@ class MessageView(APIView):
             )
 
         elif Message.objects.filter(message_id=pk).exists():
-            """Lấy thông tin chi tiết một tin nhắn"""
+            """Lấy chi tiết một tin nhắn"""
             message = get_object_or_404(Message, message_id=pk)
 
             # Kiểm tra quyền tham gia
@@ -206,7 +251,7 @@ class MessageView(APIView):
             )
 
     def post(self, request, pk):
-        """Gửi một tin nhắn vào phòng chat"""
+        """Gửi tin nhắn vào phòng chat"""
         chatroom = get_object_or_404(ChatRoom, chatroom_id=pk)
 
         # Kiểm tra quyền tham gia
