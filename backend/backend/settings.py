7f94152f48a54b548fb2e1d77f909ecf71b04699
NhatQuang1501 <nhatquang1512003@gmail.com>
2025-01-02 00:32:16 +0700

    fix: allow all cors origin

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -31,7 +31,7 @@ SECRET_KEY = os.getenv("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ["localhost", "127.0.0.1", "165.232.170.169"]
+ALLOWED_HOSTS = ["localhost", "127.0.0.1", "165.232.170.169", "*"]
 
 
 # Application definition
