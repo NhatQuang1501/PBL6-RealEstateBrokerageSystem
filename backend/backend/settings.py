3fa46680ba94d8049c0037894050378ce40a9d8d
NhatQuang1501 <nhatquang1512003@gmail.com>
2025-01-01 10:31:01 +0700

    fix: avatar location in production server

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -147,7 +147,7 @@ STATIC_URL = "static/"
 STATIC_ROOT = os.path.join(BASE_DIR, "static/")
 
 MEDIA_URL = "media/"
-MEDIA_ROOT = os.path.join(BASE_DIR, "media/")
+MEDIA_ROOT = "/var/www/pbl6/media/"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
